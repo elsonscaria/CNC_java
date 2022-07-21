5779cfc5fe47ee986165e0f55bd95c0729a59eca
elsonscaria <elsonscaria77@gmail.com>
2022-07-20 23:00:09 -0500

    Java Files Added

---
+++ Eight.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Eight {
+
+	public static void main(String[] args) {
+		int i=50;
+		do {
+			if (i%2!=0) {
+				System.out.println(i);S
+			}
+			i++;
+		}while(i<=100);
+		
+	}
+
+}
---
+++ Two.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Two {
+
+	public static void main(String[] args) {
+		int i=Integer.parseInt(args[0]);
+		if (i%2==0) {
+			System.out.println("Even Number");
+		}
+		else {
+			System.out.println("Odd Number");
+		}
+	}
+
+}
